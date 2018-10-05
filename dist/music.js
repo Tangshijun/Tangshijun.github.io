6aba970e70bb97e21c3d71886a5808ef7a80754e
Tangshijun <835294201@qq.com>
2018-10-05 14:50:52 +0800

    Site updated: 2018-10-05 14:50:50

---
+++ dist/music.js
@@ -0,0 +1,29 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    fixed: true,
+    autoplay: false,
+    audio: [
+        {
+            name: '���or���', //��Ƶ����
+            artist: '�ܸ�', //��Ƶ������
+            url: '/resources/music/�˷۵�̨�������� - ���or��롪������.mp3', //��Ƶ����
+            cover: '/resources/music/�ϵ�.jpg', //��Ƶ����
+            lrc: '0', //��Ƶ��ʣ���������lrcTypeʹ��
+            theme: '#46718b' //�л�������Ƶʱ������ɫ��������� theme ���ȼ���
+        },
+        {
+            name: 'Time', //���ֻ��һ�׸裬ɾ����һ�飬���и���������˸�ʽ���������
+            artist: '�ܸ�',
+            url: '/resources/music/�˷۵�̨�������� - Time����H2g����.mp3',
+            cover: '/resources/music/�ϵ�.jpg',
+            theme: '#46718b'
+        }
+        {
+            name: 'spoiler', //���ֻ��һ�׸裬ɾ����һ�飬���и���������˸�ʽ���������
+            artist: '�Լ�',
+            url: '/resources/music/��̰�_����.wav',
+            cover: '/resources/music/�ϵ�.jpg',
+            theme: '#46718b'
+        }
+    ]
+});
