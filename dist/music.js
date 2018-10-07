19e7387675ab91265f5904f14d5ecca007a8ec27
Tangshijun <835294201@qq.com>
2018-10-07 13:38:12 +0800

    Site updated: 2018-10-07 13:38:10

--- dist/music.js
+++ dist/music.js
@@ -5,22 +5,22 @@ const ap = new APlayer({
     lrcType: 0,
     audio: [
         {
-            name: '���or���', //��Ƶ����
+            name: '����or����', //��Ƶ����
             artist: '�ܸ�', //��Ƶ������
-            url: 'https://v2.fangcloud.com/share/8703ea8632f3408afecae7463c', //��Ƶ����
+            url: '<iframe frameborder="no" border="0" marginwidth="0" marginheight="0" width=330 height=86 src="//music.163.com/outchain/player?type=3&id=908548853&auto=1&height=66"></iframe>', //��Ƶ����
             cover: '/resources/music/�ϵ�.jpg', //��Ƶ����
-            lrc: '0', //��Ƶ��ʣ���������lrcTypeʹ��
-            theme: '#46718b' //�л�������Ƶʱ������ɫ��������� theme ���ȼ���
+            lrc: '0', //��Ƶ���ʣ�����������lrcTypeʹ��
+            theme: '#46718b' //�л�������Ƶʱ������ɫ���������� theme ���ȼ���
         },
         {
-            name: 'Time', //���ֻ��һ�׸裬ɾ����һ�飬���и���������˸�ʽ���������
+            name: 'Time', //����ֻ��һ�׸裬ɾ����һ�飬���и����������˸�ʽ������������
             artist: '�ܸ�',
             url: 'https://v2.fangcloud.com/share/8703ea8632f3408afecae7463c',
             cover: '/resources/music/�ϵ�.jpg',
             theme: '#46718b'
         },
         {
-            name: 'spoiler', //���ֻ��һ�׸裬ɾ����һ�飬���и���������˸�ʽ���������
+            name: 'spoiler', //����ֻ��һ�׸裬ɾ����һ�飬���и����������˸�ʽ������������
             artist: '�Լ�',
             url: 'https://v2.fangcloud.com/share/8703ea8632f3408afecae7463c',
             cover: '/resources/music/�ϵ�.jpg',
