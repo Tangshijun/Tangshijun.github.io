aa086ba308b851e3813fbb7de05f3abe93aa3d4d
Tangshijun <835294201@qq.com>
2018-10-21 15:18:32 +0800

    Site updated: 2018-10-21 15:18:31

--- dist/music.js
+++ dist/music.js
@@ -5,25 +5,19 @@ const ap = new APlayer({
     lrcType: 0,
     audio: [
         {
-            name: '����or����', //��Ƶ����
-            artist: '�ܸ�', //��Ƶ������
-            url: '<iframe frameborder="no" border="0" marginwidth="0" marginheight="0" width=330 height=86 src="//music.163.com/outchain/player?type=3&id=908548853&auto=1&height=66"></iframe>', //��Ƶ����
-            cover: '/resources/music/�ϵ�.jpg', //��Ƶ����
-            lrc: '0', //��Ƶ���ʣ�����������lrcTypeʹ��
-            theme: '#46718b' //�л�������Ƶʱ������ɫ���������� theme ���ȼ���
+            name: '告诫',
+            artist: '孤儿们',
+            url: 'http://musiclin.test.upcdn.net/gaojiegaojie.mp3',
+            cover: 'cover1.jpg',
+            lrc: 'lrc1.lrc',
+            theme: '#ebd0c2'
         },
         {
-            name: 'Time', //����ֻ��һ�׸裬ɾ����һ�飬���и����������˸�ʽ������������
-            artist: '�ܸ�',
-            url: 'https://v2.fangcloud.com/share/8703ea8632f3408afecae7463c',
-            cover: '/resources/music/�ϵ�.jpg',
-            theme: '#46718b'
-        },
-        {
-            name: 'spoiler', //����ֻ��һ�׸裬ɾ����һ�飬���и����������˸�ʽ������������
-            artist: '�Լ�',
-            url: 'https://v2.fangcloud.com/share/8703ea8632f3408afecae7463c',
-            cover: '/resources/music/�ϵ�.jpg',
+            name: '自白',
+            artist: '孤儿们',
+            url: 'http://musiclin.test.upcdn.net/zb.mp3',
+            cover: 'cover2.jpg',
+            lrc: 'lrc2.lrc',
             theme: '#46718b'
         }
     ]
